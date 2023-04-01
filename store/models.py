79ffa4bedc73ee4eecf6f853da6c78c2eb1127c1
AliHamza005 <ali.hamza@appifytech.com>
2023-04-01 14:54:14 +0500

    New API are added for managing cart

--- store/models.py
+++ store/models.py
@@ -78,7 +78,7 @@ class OrderItem(models.Model):
     unit_price = models.DecimalField(max_digits=6 , decimal_places=2)
     
 class Cart(models.Model):
-    id = models.UUIDField(primary_key=True,)
+    id = models.UUIDField(primary_key=True,default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
  
 class CartItem(models.Model):
